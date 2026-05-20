235e1746c1a9a28bebe80c34b9ac1d48672b7254
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-20 17:51:18 +0900

    ファーストコミット

---
+++ moaattravel2/src/main/java/com/example/moattravel2/Moattravel2Application.java
@@ -0,0 +1,13 @@
+package com.example.moattravel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moattravel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moattravel2Application.class, args);
+	}
+
+}

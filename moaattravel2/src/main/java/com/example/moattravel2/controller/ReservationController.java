044014055ad2d535b59d50cebaeced07dcdd494d
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-25 12:39:37 +0900

    inputメソッドの追加

--- moaattravel2/src/main/java/com/example/moattravel2/controller/ReservationController.java
+++ moaattravel2/src/main/java/com/example/moattravel2/controller/ReservationController.java
@@ -7,20 +7,34 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.moattravel2.entity.House;
 import com.example.moattravel2.entity.Reservation;
 import com.example.moattravel2.entity.User;
+import com.example.moattravel2.form.ReservatonInputForm;
+import com.example.moattravel2.repository.HouseRepository;
 import com.example.moattravel2.repository.ReservationRepository;
 import com.example.moattravel2.security.UserDetailsImpl;
+import com.example.moattravel2.service.ReservationService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class Reservationcontroller {
+public class ReservationController {
 	
 	private final ReservationRepository reservationRepository;
+	
+	private final ReservationService reservationService;
+	
+	private final HouseRepository houseRepository;
+	
 	
 	@GetMapping("/reservations")
 	public String index(@AuthenticationPrincipal UserDetailsImpl userDetailImpl, @PageableDefault(page=0, size = 10, sort ="id",direction =Direction.ASC)Pageable pageable, Model model) {
@@ -33,6 +47,25 @@ public class Reservationcontroller {
 		
 		return "reservations/index";
 	}
+	
+	@GetMapping("/houses/{id}/reservations/input")
+	public String input(
+			@PathVariable(name ="id")Integer id,
+			@ModelAttribute @Validated ReservatonInputForm reservationInputForm,
+			BindingResult bindingResult,
+			RedirectAttributes redirectAttributes,
+			Model model
+			) {
+		
+		House house = houseRepository.getReferenceById(id);
+		
+		Integer numberOfPeople = reservationInputForm.getNumberOfPeople();
+		
+		Integer capacity =house.getCapacity();
+		
+		return "redirect:/houses/{id}/reservations/confirm";
+	}
+	
 	
 
 }
---
+++ moaattravel2/src/main/java/com/example/moattravel2/service/ReservationService.java
@@ -0,0 +1,5 @@
+package com.example.moattravel2.service;
+
+public class ReservationService {
+
+}
